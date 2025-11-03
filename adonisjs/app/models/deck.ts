78d790981f3c95e4b61284154d7858f41ef92796
Brendan Fleurdelys <158816892+Fbrend23@users.noreply.github.com>
2025-11-03 09:09:41 +0100

    feat(deck) Add model and migration

---
+++ adonisjs/app/models/deck.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasOne } from '@adonisjs/lucid/orm'
+import type { HasOne } from '@adonisjs/lucid/types/relations'
+import User from './user.js'
+
+export default class Deck extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare title: string
+
+  @column()
+  declare description: string
+
+  @column()
+  declare isPublished: boolean
+
+  @column()
+  declare userId: number
+  //relations
+  @hasOne(() => User)
+  declare user: HasOne<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
