dca95dd295d2198c700038a32936598e5722fb2c
Brendan Fleurdelys <158816892+Fbrend23@users.noreply.github.com>
2025-11-10 09:09:01 +0100

    feat(Routes) Add Edge render

--- adonisjs/start/routes.ts
+++ adonisjs/start/routes.ts
@@ -11,6 +11,10 @@ import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
 const AuthController = () => import('#controllers/auth_controller')
 
+router.get('/', async ({ view }) => {
+  return view.render('home')
+})
+
 router
   .group(() => {
     router.post('/register', [AuthController, 'register'])
