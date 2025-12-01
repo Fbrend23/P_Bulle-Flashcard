83f8cabaae7bcb14f6edf0572b80dd6542fa441b
Brendan Fleurdelys <pl76tup@eduvaud.ch>
2025-12-01 10:53:49 +0100

    feat(routes): add routes for flashcards

--- adonisjs/start/routes.ts
+++ adonisjs/start/routes.ts
@@ -9,6 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+import CardsController from '#controllers/flashcards_controller'
 const HomepagesController = () => import('#controllers/homepages_controller')
 const DecksController = () => import('#controllers/decks_controller')
 const AuthController = () => import('#controllers/auth_controller')
@@ -26,3 +27,16 @@ router.get('/decks/:id/edit', [DecksController, 'edit']).as('deck.edit')
 router.post('/decks/:id/update', [DecksController, 'update']).as('deck.update')
 //destroy
 router.get('/decks/:id/destroy', [DecksController, 'destroy']).as('deck.destroy')
+
+//Cards
+router
+  .group(() => {
+    router.get('/', [CardsController, 'index']).as('card.index')
+    router.get('/show', [CardsController, 'show']).as('card.show')
+    router.get('/create', [CardsController, 'create']).as('card.create')
+    router.post('/', [CardsController, 'store']).as('card.store')
+    router.get('/:id/edit', [CardsController, 'edit']).as('card.edit')
+    router.post('/:id', [CardsController, 'update']).as('card.update')
+    router.get('/:id/destroy', [CardsController, 'destroy']).as('card.destroy')
+  })
+  .prefix('/decks/:deckId/cards')
